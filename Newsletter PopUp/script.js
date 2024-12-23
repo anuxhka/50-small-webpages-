4fb43e19c137d20e1e18630cc648cbdef90db4f6
Anushka <137384182+anushkea@users.noreply.github.com>
2024-12-23 09:22:02 +0530

    Add files via upload

---
+++ Newsletter PopUp/script.js
@@ -0,0 +1,6 @@
+
+// making a function which will toggle the class
+function toggleNewsletter(){
+    const newsletter = document.getElementById("newsletter");
+    newsletter.classList.toggle("active");
+}
